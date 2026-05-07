c5c508f82a2acf34f24e047f4ca2a8e1e1a034d8
edlook <advertisernet@storecategory.com>
2026-05-07 14:28:52 +0200

    Create HowItWorksPage component with layout

---
+++ src/pages/HowItWorks.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header';
+import HowItWorks from '@/components/HowItWorks';
+import Footer from '@/components/Footer';
+
+const HowItWorksPage = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Header />
+      <HowItWorks />
+      <Footer />
+    </div>
+  );
+};
+
+export default HowItWorksPage;
